2bd57c6b9561a3d58a16ec2f6879273c5f50ea8b
Clayton Ray <iamclaytonray@gmail.com>
2019-04-15 14:34:12 -0400

    feat: add new favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Clayton Ray`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/images/sdb-logo.png`, // This path is relative to the root of the site.
+        icon: `src/assets/icons/favicon.ico`,
       },
     },
     // 'gatsby-plugin-sitemap',
